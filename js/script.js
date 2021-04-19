3eced28f47d1453eebff10c17378372cce4473ec
Filippo Garota <filogarota101@gmail.com>
2021-04-19 16:58:51 +0200

    Stampa dinamica dei link dell'header

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    navItems: [
+      "Home",
+      "Pages",
+      "Portfolio",
+      "Blog",
+      "Shop",
+      "Elements",
+      "svg/svg-1.svg"
+    ]
+  
+  },
+
+
+})
